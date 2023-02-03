32b04e1ad92a3b16274b8d2c9b65bea00243751e
ilarioforcherio <ilarioforcherio@gmail.com>
2023-02-03 10:30:55 +0100

    1-sec-response

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -6,25 +6,33 @@
                 <!-- prima-sezione -->
                 <div class="m-auto w-75 container-sm d-flex justify-content-center">
                     <div class="row m-auto">
-                        <div class=" img col col-md-6  col-lg-6">
-
-                            <img class="img-fluid object-fit-contain"  src="../assets/img/io.jpeg" alt="">
-                            <div class="txt pt-4" >Web Developer</div>
+                        <div class="img col-sm-12 col-md-6  col-lg-6">
+    
+                            <img class="img-fluid object-fit-contain" src="../assets/img/io.jpeg" alt="">
+                            
                         </div>
                         <!-- sezione-text -->
                         <div class="col col-md-6  col-lg-6  txt">
                             <!-- text -->
                             <div class="row m-auto">
                                 <div class="col">
-                                    <h1 class="txt">Hi, Nice to meet you.I’m</h1>
+                                    <h1 class="txt">Hi, Nice to meet you.I'm</h1>
+                                   
+                                    <!-- i-f -->
+                                    <div class="row">
+                                        <div class="col ">
+                                            <h2 class="i-f">ILARIO <span>FORCHERIO</span></h2>
+                                        </div>
+                                    </div>
+                                   <!-- web-developer -->
+                                    <div class="row">
+                                        <div class="col">
+                                            <h2 class="w-developer txt">Web Developer</h2>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <!-- i-f -->
-                            <div class="row">
-                                <div class="col ">
-                                    <h2 class="i-f">ILARIO <span>FORCHERIO</span></h2>
-                                </div>
-                            </div>
+    
                         </div>
                     </div>
                 </div>
@@ -93,11 +101,11 @@ export default {
 
 
 <style lang="scss" scoped>
-
 main {
     // width: 100%;
     padding-top: 100px;
 }
+
 .section {
     display: flex;
     justify-content: center;
@@ -106,10 +114,10 @@ main {
     width: 100%;
     height: 100vh;
 }
-.img img{
+
+.img img {
     width: 100%;
 }
-
 
 .b-w {
     border: 1px solid white;
@@ -118,23 +126,29 @@ main {
 .txt {
     //text-align: justify;
     color: white;
-     font-size: 7rem;
+    font-size: 5.5rem;
     font-family: Helvetica Neue;
     font-weight: bold;
-    line-height: 5.5rem;
+    line-height: 4.5rem;
 }
 
 .i-f {
     // justify;
     letter-spacing: 0.4rem;
     padding-top: 20px;
-    color: orange;
-    font-size: 2rem;
-    font-family:  'Oswald', sans-serif;
+    color: orange; // font-size: 2rem;
+    font-family: 'Oswald', sans-serif;
     font-weight: bold;
     overflow-wrap: normal;
     line-height: 3.5rem; //inline-size: 250px;
     // overflow-wrap: break-word;
+}
+.w-developer{
+    line-height: 3.5rem;
+    font-size: 4rem;
+     //inline-size: 300px;
+     //overflow-wrap: break-word;
+
 }
 
 button {
