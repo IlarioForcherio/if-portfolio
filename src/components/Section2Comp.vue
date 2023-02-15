b62f688d9c2d48f9a9b909333c84b81dc5848d58
ilarioforcherio <ilarioforcherio@gmail.com>
2023-02-15 11:43:27 +0100

    kk

--- src/components/Section2Comp.vue
+++ src/components/Section2Comp.vue
@@ -102,7 +102,7 @@ export default {
     font-family: Helvetica Neue;
     font-weight: bold;
     line-height: 4rem;
-    padding-right: 80px;
+    padding-right: 100px;
 }
 .bw {
     border: 1px solid rgb(107, 106, 106);
