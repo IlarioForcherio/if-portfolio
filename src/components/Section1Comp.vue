bb3adc20a0bea4a9b8457b286d34885f1500623a
ilarioforcherio <ilarioforcherio@gmail.com>
2023-02-21 20:25:24 +0100

    we

--- src/components/Section1Comp.vue
+++ src/components/Section1Comp.vue
@@ -10,7 +10,7 @@
                         <div class="col-lg-12">
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <h2 class=" txt">Hi, nice to meet you, my name is</h2>
+                                    <h2 class=" txt">Hiwe, nice to meet you, my name is</h2>
                                 </div>
                             </div>
 
